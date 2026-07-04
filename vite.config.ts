0927ab8503ee8b3cee925d002daa2b80766a32a9
simonas31 <babarskas.simonas@gmail.com>
2026-07-04 16:18:26 +0300

    vite.config.ts fix

--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,23 @@
 import { defineConfig } from "vite";
 import laravel from "laravel-vite-plugin";
 import tailwindcss from "@tailwindcss/vite";
+import vue from "@vitejs/plugin-vue";
+import path from "path";
 
 export default defineConfig({
- plugins: [
+  plugins: [
     laravel({
       input: ["resources/js/app.ts"],
       refresh: true,
     }),
-    vue(),
+    vue({
+      template: {
+        transformAssetUrls: {
+          base: null,
+          includeAbsolute: false,
+        },
+      },
+    }),
     tailwindcss(),
   ],
   resolve: {
